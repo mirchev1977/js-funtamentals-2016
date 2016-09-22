95189a78ab71daf91f135949e4e853ec88fdbd66
Miroslav <mirchev1977@novihorizonti.eu>
2016-09-22 10:31:46 +0300

    fruit or vegetable

--- 02-control-flow-logic/08-fruit-or-vegetable.js
+++ 02-control-flow-logic/08-fruit-or-vegetable.js
@@ -1,1 +1,28 @@
+"use strict";
 
+solve(["pepper"]);
+
+function solve([input]){
+    let fruites = ["banana", "apple", "kiwi", "cherry", "lemon", "grapes", "peach"];
+    let vegetables = ["tomato", "cucumber", "pepper", "onion", "garlic", "parsley"];
+
+    let foundFruits = fruites.filter(function (fruit) {
+            return input === fruit;
+    });
+
+    let foundVegetables = vegetables.filter(function (veg) {
+            return input === veg;
+    });
+
+    if(foundFruits.length > 0){
+        console.log("fruit");
+        return;
+    }
+
+    if(foundVegetables.length > 0){
+        console.log("vegetable");
+        return;
+    }
+
+    console.log("unknown");
+}
