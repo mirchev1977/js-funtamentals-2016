02c3acb653225b23e57b0fc7739830ea79879cf0
Miroslav <mirchev1977@novihorizonti.eu>
2016-09-30 01:38:19 +0300

    monthly calendar

---
+++ 04-conditions-and-loops/src/09-monthly-calendar.js
@@ -0,0 +1,75 @@
+"use strict";
+
+solve(['24', '12', '2012']);
+
+function solve (input) {
+	let [day, month, year] = input.map(Number);
+
+	let dt = new Date(year, month - 1, day);
+
+	let year1 = dt.getFullYear();
+	let month1 = dt.getMonth() + 1;
+	let date1 = dt.getDate();
+	let day1 = dt.getDay();
+
+	let dt2 = new Date(year, month1, 0);
+
+	let year2 = dt2.getFullYear();
+	let month2 = dt2.getMonth() + 1;
+	let date2 = dt2.getDate();
+	let day2 = dt2.getDay();
+
+	let calendar = []
+	let arr = ['', '', '', '', '', '', ''];
+
+	for (let i = 1; i <= date2; i++) {
+		let dt3 = new Date(year, month - 1, i);
+		
+		let year3 = dt3.getFullYear();
+		let month3 = dt3.getMonth() + 1;
+		let date3 = dt3.getDate();
+		let day3 = dt3.getDay();
+
+
+		if (day3 === 0) {
+			calendar.push(arr);
+			arr = ['', '', '', '', '', '', ''];
+		}
+		if (date3 === date1) {
+			arr[day3] = `<td class="today">${date3}</td>`;
+		} else {
+			arr[day3] = `<td>${date3}</td>`;
+		}
+	}
+	calendar.push(arr);
+
+	//previous month
+	let previousMonth = new Date(year, month - 1, 1);
+
+	let first = calendar[0];
+
+	for (let i = first.length - 1; i >= 0; i--) {
+		if (first[i] === '') {
+			previousMonth.setDate(previousMonth.getDate() - 1);
+			first[i] = `<td class="prev-month">${previousMonth.getDate()}</td>`;
+		}
+	}
+
+	//next month
+	let nextMonth = new Date(year, month, 1);
+	let last = calendar[calendar.length - 1];
+
+	for(let i in last){
+		if(last[i] === ''){
+			last[i] = `<td class="next-month">${nextMonth.getDate()}</td>`;
+			nextMonth.setDate(nextMonth.getDate() + 1);
+		}
+	}
+	
+	console.log('<table>');
+	console.log('<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>');
+	for(let row of calendar){
+		console.log(`<tr>${row[0]}${row[1]}${row[2]}${row[3]}${row[4]}${row[5]}${row[6]}</tr>`);
+	}
+	console.log('</table>');
+}
