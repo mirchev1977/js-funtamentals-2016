54058197b0b559bcdc27af35484e3d4c8be3e2b3
Miroslav <mirchev1977@novihorizonti.eu>
2016-10-06 16:21:44 +0300

    find variable names in sentences

---
+++ 07-strings-and-reg-exp/e07-find-variable-names-in-sentences.js
@@ -0,0 +1,19 @@
+"use strict";
+
+solve(["Calculate the _area of the _perfectRectangle object."]);
+
+
+function solve (input) {
+	let [sentence] = input;
+
+	let pattern = /[^_\w]+_[a-zA-Z]+\b/g;
+
+	let matches = sentence.match(pattern);
+
+	matches.forEach( function(element, index) {
+		matches[index] = element.replace(/_/, '').trim();
+	});
+
+
+	console.log(matches.join(','));
+}
